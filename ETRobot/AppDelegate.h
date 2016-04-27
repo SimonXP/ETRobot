c8c2c8229646dbd4961a45db53ea84820f00aef5
allenXP <935329967@qq.com>
2016-04-27 16:53:51 +0800

    Initial Commit

---
+++ ETRobot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ETRobot
+//
+//  Created by IHOME on 16/4/27.
+//  Copyright © 2016年 IHOME. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ETRobot/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ETRobot
+//
+//  Created by IHOME on 16/4/27.
+//  Copyright © 2016年 IHOME. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
